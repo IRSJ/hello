3b997964c67f9d3806cfabb7bcb03140c4a6fb9a
Aluno <Aluno@DESKTOP-0S24855>
2017-10-26 21:30:09 -0300

    oie

---
+++ oie/src/oie/HelloMundo.java
@@ -0,0 +1,15 @@
+package oie;
+
+import java.util.Scanner;
+
+public class HelloMundo {
+
+	public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+	String nome = sc.nextLine();
+	System.out.println("Seja bem vindo: " + nome);
+	}
+	
+	
+
+}
